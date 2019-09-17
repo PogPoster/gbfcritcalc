93a572202326d55592231b51b3589d317067e82a
= <megamanzxcv@gmail.com>
2019-09-18 00:28:46 +1000

    Updates

---
+++ precache-manifest.c542b578dba984398f780b5a2fa1f3f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d49279e56cbb86fd213fa2124f619a",
+    "url": "/gbfcritcalc/index.html"
+  },
+  {
+    "revision": "2e30efd85491af0c8764",
+    "url": "/gbfcritcalc/static/css/main.b5897f18.chunk.css"
+  },
+  {
+    "revision": "78edbddba514069a819f",
+    "url": "/gbfcritcalc/static/js/2.dbce9a62.chunk.js"
+  },
+  {
+    "revision": "2e30efd85491af0c8764",
+    "url": "/gbfcritcalc/static/js/main.f00234d2.chunk.js"
+  },
+  {
+    "revision": "554329b53c5f68230b10",
+    "url": "/gbfcritcalc/static/js/runtime~main.74485640.js"
+  }
+]);
