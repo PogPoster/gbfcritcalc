5674b1957c6f631a86466e5cd3c2d2bb10b91f3f
PogPoster <megamanzxcv@gmail.com>
2021-04-01 00:58:00 +1100

    fixed magna summons

--- src/Component/Summons.js
+++ src/Component/Summons.js
@@ -13,8 +13,8 @@ export class Summons extends Component {
                 <input type="radio" name="summons" value={3.8} onChange={this.props.click}/> 1 Demi + 1 Primal ULB<br/>
                 <input type="radio" name="summons" value={3.7} onChange={this.props.click}/> 1 Demi + 1 Primal FLB<br/>
                 <input type="radio" name="summons" value={2.3} onChange={this.props.click}/> 1 Demi<br/>
-                <input type="radio" name="summons" value={3.4} onChange={this.props.click}/> 2 magna<br/>
-                <input type="radio" name="summons" value={2.2} onChange={this.props.click}/> 1 magna<br/>
+                <input type="radio" name="summons" value={3.8} onChange={this.props.click}/> 2 magna<br/>
+                <input type="radio" name="summons" value={2.4} onChange={this.props.click}/> 1 magna<br/>
 
             </div>
         )
